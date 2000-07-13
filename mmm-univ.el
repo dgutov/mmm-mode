e632aecf696cfe4efc4d75a8d151d20fdbaf2723
mas <mas>
2000-07-13 02:49:56 +0000

    Limited matches to letter/dash strings that are fboundp.

--- mmm-univ.el
+++ mmm-univ.el
@@ -34,22 +34,32 @@
 
 (require 'mmm-auto)
 
+(defun mmm-univ-get-mode (string)
+  (string-match "[a-zA-Z-]+" string)
+  (let ((modestr (match-string 0 string)))
+    (and modestr
+         (intern (if (equal (substring modestr -5) "-mode")
+                     modestr
+                   (concat modestr "-mode"))))))
+
 (mmm-add-classes
  `((universal
-    :front "{%\\([^]]+\\)%}"
+    :front "{%\\([a-zA-Z-]+\\)%}"
     :back "{%/~1%}"
     :insert ((?/ universal "Submode: " @ "{%" str "%}" @ "\n" _ "\n"
                  @ "{%/" str "%}" @))
-    :match-submode ,#'(lambda (str)
-                        (string-match "[a-zA-Z-]+" str)
-                        (let ((modestr (match-string 0 str)))
-                          (unless modestr (error "Something is screwy."))
-                          (if (equal (substring modestr -5) "-mode")
-                              (intern modestr)
-                            (intern (concat modestr "-mode")))))
+    :front-verify ,#'(lambda ()
+                       (fboundp
+                        (mmm-univ-get-mode (match-string 0))))
+    :match-submode mmm-univ-get-mode
     :save-matches 1
     )))
 
 (provide 'mmm-univ)
+
+
+;;; Local Variables:
+;;; mmm-global-classes: nil
+;;; End:
 
 ;;; mmm-univ.el ends here
