96906747e862449b722b6e2c741b61871894ac3a
Dmitry Gutov <dmitry@gutov.dev>
2023-09-17 03:28:37 +0300

    Release 0.5.10

--- mmm-vars.el
+++ mmm-vars.el
@@ -1062,7 +1062,7 @@ The CLASSES are all made private, i.e. non-user-visible."
 ;;}}}
 ;;{{{ Version Number
 
-(defconst mmm-version "0.5.8"
+(defconst mmm-version "0.5.10"
   "Current version of MMM Mode.")
 
 (defun mmm-version ()
